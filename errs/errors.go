7dbc8216a5b4ef29abaf6f7dc5d0767d1904ee65
Guanyun <guanyun1024@icloud.com>
2021-09-12 11:15:29 +0800

    update

---
+++ errs/errors.go
@@ -0,0 +1,35 @@
+package errs
+
+import "fmt"
+
+type commonErr struct {
+	code int
+	msg  string
+}
+
+func (e commonErr) Error() string {
+	return fmt.Sprintf("code:%d,msg:%v", e.code, e.msg)
+}
+
+func New(code int, msg string) error {
+	return commonErr{
+		code: code,
+		msg:  msg,
+	}
+}
+
+func GetCode(err error) int {
+	if e, ok := err.(commonErr); ok {
+		return e.code
+	}
+
+	return -1
+}
+
+func GetMsg(err error) string {
+	if e, ok := err.(commonErr); ok {
+		return e.msg
+	}
+
+	return err.Error()
+}
